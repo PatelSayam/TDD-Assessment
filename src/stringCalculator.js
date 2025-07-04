eb55fc4220635f6327496b61bf59f45a7c0dbf2a
Sayam Patel <sayamparejiya@gmail.com>
2025-07-05 02:21:24 +0530

    [Test-4] Added Pass code of handle custom delimiters

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,9 +1,20 @@
 function add (str) {
 
     if(str === "")  return 0;
-    
+
+    // default delimiters
+    let delimiters = [",", "\n"];
+
+    if(str.startsWith("//")) {
+        let delimeter = str.substring(2,3);
+        delimiters.push(delimeter);
+        str = str.substring(4);
+    }
+
+    const regex = new RegExp(`[${delimiters.join("")}]`);
+
     return str
-    .split(/[\n,]+/)
+    .split(regex)
     .map(s => s.trim())
     .filter(s => s !== "")
     .reduce((sum, num) => sum + Number(num), 0);
