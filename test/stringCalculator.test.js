5d2dad53e4233fab07ea75d2c9ffef88a4dccc22
Sayam Patel <sayamparejiya@gmail.com>
2025-07-05 01:00:05 +0530

    [Test-4] Added Failed test case of handling custome delimeters

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -24,4 +24,9 @@ describe("String Calculator Addition", () => {
         expect(add("1\n2,3")).toBe(6);
         expect(add("1\n2\n3")).toBe(6);
     })
+
+    test('should handle custome delimeters', () => {
+        expect(add("//;\n1;2")).toBe(3);
+        expect(add("//;\n3;4\n5;6")).toBe(18);  
+    })
 });
